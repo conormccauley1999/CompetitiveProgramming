41e4d967b1a466bd8122c83a558ca3e01dd07869
conormccauley1999 <conormccauley1999@gmail.com>
2019-02-26 09:05:20 +0000

    Problem 7 finished

--- 0-50/7.py
+++ 0-50/7.py
@@ -1,1 +1,25 @@
 # Problem 7
+
+pn = 10001
+pf = 1
+
+ps = set([2])
+
+n = 3
+
+while pf < pn:
+
+	isp = True
+
+	for p in ps:
+		if n % p == 0:
+			isp = False
+			break
+
+	if isp:
+		ps.add(n)
+		pf += 1
+
+	n += 2
+
+print n - 2
