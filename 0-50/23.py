e6343b33ed9895c57305d30c3ac0ee0761c48af4
conormccauley1999 <conormccauley1999@gmail.com>
2019-02-27 13:27:19 +0000

    Added 23

---
+++ 0-50/23.py
@@ -0,0 +1,21 @@
+# Problem 23
+
+from common import *
+
+a = []
+r = [True] * 28123
+
+for n in range(1, 28124):
+	if getDivisorSum(n) > n: a.append(n)
+
+for x in range(0, len(a)):
+	for y in range(0, len(a)):
+		s = a[x] + a[y]
+		if s <= 28123: r[s - 1] = False
+
+s = 0
+
+for i in range(0, len(r)):
+	if r[i]: s += (i + 1)
+
+print s
--- 0-50/common.py
+++ 0-50/common.py
@@ -61,7 +61,7 @@ def getDivisorCount(n):
 
 def getDivisorSum(n):
 	s = 0
-	for i in range(1, int(n / 2) + 1):
+	for i in range(1, (n / 2) + 1, 1):
 		if n % i == 0: s += i
 	return s
 
