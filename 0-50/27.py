45b81f563feb4d0556e2fcf8f3b3572d0ed80a90
conormccauley1999 <conormccauley1999@gmail.com>
2019-03-01 08:20:31 +0000

    Added p27

--- 0-50/27.py
+++ 0-50/27.py
@@ -2,21 +2,36 @@
 
 from common import *
 
-print len(primesLessThan(1000))
+ps = set()
 
-"""
-maxN = 0
-prod = 0
+def main():
 
-for b in range(-1000, 1001):
-    if not isPrime(b): continue
-    for a in range(-999, 1000):
-        n, x = 0, b
-        while isPrime(x):
-            x = (n * n) + (a * n) + b
-            n += 1
-        if n > maxN:
-            maxN, prod = n, a * b
+	s, p = 0, 0
 
-print prod
-"""
+	a = -999
+	while a < 1000:
+		b = -1000
+		while b <= 1000:
+			n = 0
+			while True:
+				x = (n * n) + (a * n) + b
+				if isPrimeCache(x):
+					if n > s:
+						s = n
+						p = a * b
+				else: break
+				n += 1
+			b += 1
+		a += 1
+
+	return p
+
+def isPrimeCache(n):
+	if n < 0: return False
+	elif n in ps: return True
+	elif isPrime(n):
+		ps.add(n)
+		return True
+	else: return False
+
+print main()
