b3e71f6423d203c32b6ce694cee1e265f1cc2ed3
conormccauley1999 <conormccauley1999@gmail.com>
2019-03-11 02:24:06 +0000

    Finished p64

--- 051-100/064.py
+++ 051-100/064.py
@@ -1,21 +1,22 @@
-### THIS PROBLEM IS INCOMPLETE ###
-
 # Problem 64
 
 from math import sqrt, floor
 
 def getContinuedFractionPeriod(n):
-	ls = []
-	a = floor(sqrt(n))
-	r = 1 / (sqrt(n) - a)
-	while True:
-		ls.append(a)
-		a = floor(r)
-		r = 1 / (r - a)
-	return ls
+	if int(sqrt(n)) == sqrt(n): return 0
+	m = 0
+	d = 1
+	a = a0 = floor(sqrt(n))
+	p = 0
+	while a != 2 * a0:
+		m = d * a - m
+		d = (n - (m * m)) / d
+		a = floor((a0 + m) / d)
+		p += 1
+	return p
 
 r = 0
-for n in range(2, 10000):
+for n in range(2, 10001):
 	if sqrt(n) == int(sqrt(n)): continue
-	if len(getContinuedFractionPeriod(n)) % 2 == 1: r += 1
+	if getContinuedFractionPeriod(n) % 2 == 1: r += 1
 print r
