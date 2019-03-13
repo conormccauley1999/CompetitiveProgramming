cfc05931a164aa260b824330c318c598e46cca97
conormccauley1999 <conormccauley1999@gmail.com>
2019-03-13 17:26:13 +0000

    Solved p80

--- 051-100/080.py
+++ 051-100/080.py
@@ -1,10 +1,13 @@
 # Problem 80
 
-from numpy import sqrt
+from decimal import *
+getcontext().prec = 103
 
 sm = 0
+
 for n in range(1, 100):
 	if n in [1, 4, 9, 16, 25, 36, 49, 64, 81]: continue
-	r = str(sqrt(n))[:100].replace(".", "")
+	r = str(Decimal(n).sqrt()).replace(".", "")[:100]
 	sm += sum(int(d) for d in r)
+
 print sm
