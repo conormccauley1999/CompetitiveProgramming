27b309df8b1954d84665a94deb51bbc172e94ac7
conormccauley1999 <conormccauley1999@gmail.com>
2019-03-19 06:45:27 +0000

    Folders

---
+++ 1.0-2.0/r2.py
@@ -0,0 +1,3 @@
+i = raw_input().split(" ")
+R1, M = int(i[0]), int(i[1])
+print (2 * M) - R1
---
+++ 1.0-2.0/tarifa.py
@@ -0,0 +1,6 @@
+X = int(raw_input())
+N = int(raw_input())
+t = X
+for i in range(0, N):
+	t += X - int(raw_input())
+print t
