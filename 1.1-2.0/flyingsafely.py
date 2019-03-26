3342cf452ec4d8180f4613504eee03985c132f7a
conormccauley1999 <conormccauley1999@gmail.com>
2019-03-26 23:22:20 +0000

    Finished 'Flying Safely'

--- 1.1-2.0/flyingsafely.py
+++ 1.1-2.0/flyingsafely.py
@@ -1,21 +1,9 @@
-"""def mst(p):
-	l = 0
-	visited = set()
-	stack = [p[p.keys()[0]]]
-	while len(stack) != 0:
-		node = stack.pop()
-		# to-do
-	return l
-
 t = int(raw_input())
 for _ in xrange(t):
 	n, m = map(int, raw_input().split())
-	p = {}
+	mx = 0
 	for _ in xrange(m):
 		a, b = map(int, raw_input().split())
-		if a in p: p[a].add(b)
-		else: p[a] = set([b])
-		if b in p: p[b].add(a)
-		else: p[b] = set([a])
-	print mst(p)
-"""
+		if a > mx: mx = a
+		if b > mx: mx = b
+	print mx - 1
