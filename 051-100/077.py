fc44d0ae1de1ffff83fabccbe97167a8554d37de
conormccauley1999 <conormccauley1999@gmail.com>
2019-03-27 14:54:51 +0000

    Solved p077

--- 051-100/077.py
+++ 051-100/077.py
@@ -5,6 +5,16 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from common import *
 
-mx = 10
-ps = npPrimesLessThan(mx)
+mx = 100
+ps = list(npPrimesLessThan(mx + 1))
 
+ws = [0] * (mx + 1)
+ws[0] = 1
+for i in xrange(0, len(ps)):
+	for j in xrange(ps[i], (mx + 1)):
+		ws[j] += ws[j - ps[i]]
+
+for x in range(0, (mx + 1)):
+	if ws[x] > 5000:
+		print x
+		break
