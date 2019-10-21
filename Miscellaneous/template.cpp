757a4ff5e5d07c2c0d4fc55d04653e49377b8275
unknown <conormccauley1999@gmail.com>
2019-10-21 20:46:59 +0100

    Included templates

---
+++ Miscellaneous/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define vpi vector< pair<int, int> >
+#define pi pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+using namespace std;
+
+int main() {
+
+	
+
+}
---
+++ Miscellaneous/template.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Main {
+
+	static class Reader {
+
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Reader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try { st = new StringTokenizer(br.readLine()); }
+				catch (IOException e) { e.printStackTrace(); }
+			}
+			return st.nextToken();
+		}
+
+		int ni() { return Integer.parseInt(next()); }
+		long nl() { return Long.parseLong(next()); }
+		double nd() { return Double.parseDouble(next()); }
+		
+		String n() {
+			String str = "";
+			try { str = br.readLine(); }
+			catch (IOException e) { e.printStackTrace(); }
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Reader r = new Reader();
+
+	}
+
+}
