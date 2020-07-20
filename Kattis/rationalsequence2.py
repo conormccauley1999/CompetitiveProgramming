5844b0e72e087e578ed918756d24998ad9728148
Conor Mac Amhlaoibh <conormccauley1999@gmail.com>
2020-07-20 12:49:02 +0100

    Cleaned up code

--- Kattis/rationalsequence2.py
+++ Kattis/rationalsequence2.py
@@ -1,16 +1,15 @@
 def f():
-    P, Q = map(int, list(input().split())[1].split('/'))
-    p, q = P, Q
-    t = []
+    p, q = map(int, list(input().split())[1].split('/'))
+    t = ''
     while (p, q,) != (1, 1,):
         if p > q:
             p, q = p - q, q
-            t.append('1')
+            t += '1'
         else:
             p, q = p, q - p
-            t.append('0')
-    t.append('1')
-    return int(''.join(t[::-1]), 2)
+            t += '0'
+    t += '1'
+    return int(t[::-1], 2)
 
 t = int(input())
 for i in range(t):
