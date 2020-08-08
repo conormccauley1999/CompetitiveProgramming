a24e43c0865eead1ed7e9f864384cf256738e264
Conor Mac Amhlaoibh <conormccauley1999@gmail.com>
2020-08-08 17:10:29 +0100

    PE684

---
+++ ProjectEuler/651-700/684.py
@@ -0,0 +1,16 @@
+M = 1000000007
+
+def S(k):
+    x = k // 9
+    y = k % 9
+    p = pow(10, x, M)
+    sm = (((6 * p) - 6) - (9 * x)) + ((p * (((y + 1) * (y + 2) // 2) - 1)) - y)
+    return sm
+
+f, g = 1, 2
+sm = 0
+for i in range(2, 91):
+    sm += S(f)
+    print(i, f, sm % M)
+    f, g = g, f + g
+print(sm % M)
