253637cbed4a6b14b8128499f56bec0f8a5e04fc
Conor Mac Amhlaoibh <conormccauley1999@gmail.com>
2020-09-21 16:29:26 +0100

    PE123 and PE243

--- ProjectEuler/101-150/123.py
+++ ProjectEuler/101-150/123.py
@@ -1,1 +1,16 @@
 # Problem 123
+
+from sympy import sieve
+sieve.extend_to_no(100000)
+
+ps = sieve._list
+mx = 10 ** 10
+
+for n in range(1, len(ps) + 1):
+    if not n % 1000:
+        print(n)
+    p = ps[n - 1]
+    r = (pow(p - 1, n) + pow(p + 1, n)) % pow(p, 2)
+    if r > mx:
+        print(n)
+        quit()
--- ProjectEuler/201-250/243.py
+++ ProjectEuler/201-250/243.py
@@ -1,6 +1,44 @@
 # Problem 243
 
 from sympy.ntheory import sieve
-from sympy.ntheory.factor_ import totient
 
+def calc(primes, powers):
+    n = 1
+    for i in range(COUNT):
+        n *= pow(primes[i], powers[i])
+    return n
+
+def totient(n, primes, powers):
+    t = n
+    for i in range(COUNT):
+        if not powers[i]:
+            continue
+        t *= (primes[i] - 1)
+        t //= primes[i]
+    return t
+
+COUNT = 20
+MAX_POW = 5
 R = 15499 / 94744
+
+sieve.extend_to_no(COUNT)
+primes = list(sieve._list[:COUNT])
+
+powers = [0] * 20
+cur_ix = 0
+mn = 10 ** 10
+
+mx = pow(COUNT, MAX_POW)
+for i in range(mx):
+    if not i % 10000:
+        print(i, mn, mx)
+    _i = i
+    for j in range(COUNT):
+        powers[j] = _i % MAX_POW
+        _i //= MAX_POW
+    n = calc(primes, powers)
+    if n == 1:
+        continue
+    r = totient(n, primes, powers) / (n - 1)
+    if r < R:
+        mn = min(n, mn)
